3cba39f392a5e3ca5c3e07be3a75a782262475c9
Prince-Patel84 <princegithub84@gmail.com>
2025-02-19 16:43:27 +0530

    Making Seperate Settings File

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import sys
-
+from settings import Settings
 import pygame
 
-#Game Variables
-GameSize = (1066,600)
-WindowName = "Alien Attack"
 
 class AlienAttack:
     def __init__(self):
         pygame.init()
-
-        self.screen = pygame.display.set_mode(GameSize)
-        pygame.display.set_caption("Alien Attack")
         self.clock = pygame.time.Clock()
-        self.bg_color = (230,230,230)
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(self.settings.GameSize)
+        pygame.display.set_caption(self.settings.WindowName)
 
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
             self.clock.tick(60)
 
---
+++ settings.py
@@ -0,0 +1,5 @@
+class Settings:
+    def __init__(self):
+        self.GameSize = (1066,600)
+        self.WindowName = "Alien Attack"
+        self.bg_color = (230,230,230)
