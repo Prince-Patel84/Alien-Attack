b8664830319ba063f682d06d3330a23c22ff32e7
Prince-Patel84 <princegithub84@gmail.com>
2025-02-22 13:22:52 +0530

    Ship is now Able to fire bullets

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
-
+from bullet import Bullet
 
 class AlienAttack:
     def __init__(self):
@@ -14,11 +14,13 @@ class AlienAttack:
         pygame.display.set_caption(self.settings.WindowName)
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         while True:
             self._check_events()
             self.ship.update(self)
+            self.bullets.update()
             self._update_screen()
             
             self.clock.tick(60)
@@ -49,6 +51,8 @@ class AlienAttack:
                 self.screen = pygame.display.set_mode(self.settings.GameSize)
                 pygame.display.set_caption(self.settings.WindowName)
                 self.ship.rect.midbottom = (self.ship.rect.x,self.settings.SmallGameSize[1])
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     
     def _check_keyup_events(self,event):
@@ -59,9 +63,15 @@ class AlienAttack:
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme(self)
 
         pygame.display.flip()
+
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
 if __name__ == '__main__':
     ai = AlienAttack()
--- settings.py
+++ settings.py
@@ -5,3 +5,7 @@ class Settings:
         self.WindowName = "Alien Attack"
         self.bg_color = (230,230,230)
         self.Ship_Speed = 2
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
